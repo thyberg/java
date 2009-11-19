206d42f71ca25af36bddf1881706256c72bfea51
Thomas Thyberg <thomas@thyberg.com>
2009-11-19 12:26:58 +0100

    Refactored.

--- LogFileCounter.java
+++ LogFileCounter.java
@@ -1,5 +1,5 @@
 /*
- * First naive version of http://pastebin.com/f968c762 but in java.  
+ * First naive version of http://pastebin.com/f968c762 but in java.
  */
 
 package com.thyberg.logparser;
@@ -16,68 +16,20 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 
 
 public class LogFileCounter {
+    private static final int PRINT_ITEMS = 10;
     private static final int WORKERS = 20;
 
     public static void main(final String[] args) throws IOException {
         final Reader reader = new Reader("/usr/share/dict/words");
         final List<Summer> workers = new ArrayList<Summer>(WORKERS);
-
-        final Runnable merger = new Runnable() {
-                public void run() {
-                    final Comparator<Entry<String, Integer>> c =
-                        new Comparator<Entry<String, Integer>>() {
-                            @Override
-                            public int compare(
-                                final Entry<String, Integer> o1,
-                                final Entry<String, Integer> o2
-                            ) {
-                                return o2.getValue() - o1.getValue();
-                            }
-                        };
-
-                    final Map<String, Integer> map =
-                        new HashMap<String, Integer>();
-
-                    for (final Summer s : workers) {
-                        for (
-                            final Entry<String, Integer> e :
-                            s.getMap()
-                            .entrySet()
-                        ) {
-                            Integer count = map.get(e.getKey());
-
-                            if (count == null) {
-                                count = 0;
-                                map.put(e.getKey(), count);
-                            }
-                            count += e.getValue();
-                            map.put(e.getKey(), count);
-                        }
-                    }
-
-                    final List<Entry<String, Integer>> list =
-                        new LinkedList<Entry<String, Integer>>();
-                    list.addAll(map.entrySet());
-                    Collections.sort(list, c);
-
-                    int i = 0;
-
-                    for (final Entry<String, Integer> e : list) {
-                        System.out.println(e.getKey() + " " + e.getValue());
-
-                        if (++i >= 10) {
-                            break;
-                        }
-                    }
-                }
-            };
-
-        final CyclicBarrier barrier = new CyclicBarrier(WORKERS, merger);
+        final CyclicBarrier barrier = new CyclicBarrier(
+            WORKERS,
+            new Merger(workers)
+        );
 
         for (int i = 0; i < WORKERS; i++) {
             final Summer worker = new Summer(barrier, reader);
@@ -86,8 +38,71 @@ public class LogFileCounter {
         }
     }
 
+    private static final class Merger implements Runnable {
+        private final List<Summer> workers;
+
+        public Merger(final List<Summer> workers) {
+            this.workers = workers;
+        }
+
+        public void run() {
+            final Map<String, Integer> map = new HashMap<String, Integer>();
+
+            for (final Summer s : workers) {
+                for (final Entry<String, Integer> e : s.getMap()
+                    .entrySet()
+                ) {
+                    map(map, e.getKey(), e.getValue());
+                }
+            }
+            printList(sortMap(map));
+        }
+
+        private void printList(final List<Entry<String, Integer>> list) {
+            for (int i = 0; i < PRINT_ITEMS; ++i) {
+                final Entry<String, Integer> item = list.get(i);
+                System.out.println(item.getKey() + " " + item.getValue());
+            }
+        }
+
+        private void map(
+            final Map<String, Integer> map,
+            final String key,
+            final int value
+        ) {
+            Integer count = map.get(key);
+
+            if (count == null) {
+                count = 0;
+            }
+            count += value;
+            map.put(key, count);
+        }
+
+        private List<Entry<String, Integer>> sortMap(
+            final Map<String, Integer> map
+        ) {
+            final LinkedList<Entry<String, Integer>> list =
+                new LinkedList<Entry<String, Integer>>();
+            list.addAll(map.entrySet());
+            Collections.sort(
+                list,
+                new Comparator<Entry<String, Integer>>() {
+                    @Override
+                    public int compare(
+                        final Entry<String, Integer> o1,
+                        final Entry<String, Integer> o2
+                    ) {
+                        return o2.getValue() - o1.getValue();
+                    }
+                }
+            );
+            return list;
+        }
+    }
+
     private static class Reader extends Thread {
-        final BufferedReader reader;
+        private final BufferedReader reader;
 
         public Reader(final String filename) throws FileNotFoundException {
             reader = new BufferedReader(new FileReader(filename));
@@ -112,24 +127,27 @@ public class LogFileCounter {
             return this.map;
         }
 
+        private void addLine(final String key) {
+            if (key == null) {
+                return;
+            }
+
+            Integer count = map.get(key);
+
+            if (count == null) {
+                count = 0;
+            }
+            ++count;
+            map.put(key, count);
+        }
+
         @Override
         public void run() {
             String line = null;
 
             try {
                 while ((line = reader.getLine()) != null) {
-                    final String key = extract(line);
-                    if (key == null) {
-                        continue;
-                    }
-                    Integer count = map.get(key);
-                    if (count == null) {
-                        count = 0;
-                        map.put(key, count);
-                    }
-                    ++count;
-                    map.put(key, count);
-
+                    addLine(extract(line));
                 }
             }
             catch (final IOException e) {
@@ -139,16 +157,12 @@ public class LogFileCounter {
             try {
                 barrier.await();
             }
-            catch (final InterruptedException e) {
-                e.printStackTrace();
-            }
-            catch (final BrokenBarrierException e) {
+            catch (final Exception e) {
                 e.printStackTrace();
             }
         }
 
         private String extract(final String line) {
-
             if (line.length() >= 5) {
                 return line.substring(2, 5);
             }
